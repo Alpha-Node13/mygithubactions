4ec84bccadb644b0fc4057d7bfe544e80e3178b7
Muhammad Saud <100123061+Alpha-Node13@users.noreply.github.com>
2025-09-06 23:20:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import requests
 
 # ❌ Hardcoded AWS secret 
-AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE123"
+AWS_SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
 # ❌ Hardcoded GitHub token 
 GITHUB_TOKEN = "ghp_abCDefGhijkLmnoPQRStuvWxyz1234567890"
 
 # ❌ Hardcoded Stripe API key
-STRIPE_API_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+STRIPE_API_KEY = "sk_live_1234567890abcdefghijklmnop"
 
 
 def fetch_data():
